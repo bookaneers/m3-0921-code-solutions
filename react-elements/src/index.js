919c880f04de8df9ec0dbbbc31c82815926c714d
Fernando Figueiredo <bookaneers@gmail.com>
2021-10-08 00:16:42 +0000

    Completed react-elements.

---
+++ react-elements/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var element = React.createElement(
+  'h1',
+  null,
+  'Hello, React!'
+);
+
+var $root = document.querySelector('#root');
+
+ReactDOM.render(
+  element,
+  $root,
+  React.createElement
+);
